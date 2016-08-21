634e310ca50a4118cbfc6a9a721c2489e717c85e
Adam <ad.tumi@gmail.com>
2016-08-21 22:36:12 +0200

    footer  adjustments (in progress)

--- wp-content/themes/bruczek-theme/footer.php
+++ wp-content/themes/bruczek-theme/footer.php
@@ -8,24 +8,38 @@
  *
  * @package bruczek-theme
  */
-
 ?>
 <footer class="site-footer">
-    <div class="container-fluid">
-        <div class="row content-wrapper">
+    <div class="content-wrapper">
+        <div class="container-fluid">
             <div class="footer-wrapper">
-                <div class="col-md-3">
+                <div class="col-xs-6 col-sm-4 col-md-3 col-lg-3 address-column">
                     <h3 class="footer-heading">Nasza siedziba:</h3>
-                    <p><span class="glyphicon glyphicon-map-marker phone-icon" aria-hidden="true"></span>www.bruczek.pl</p>
-                    <p>ul. Kostki brukowej 123</p>
-                    <p>22-361 Warszawa</p>
-                    <p><span class="glyphicon glyphicon-earphone phone-icon" aria-hidden="true"></span>123 123 3123</p>
+                    <div class="row">
+                        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                            <span class="grey glyphicon glyphicon-map-marker phone-icon" aria-hidden="true"></span>
+                        </div>
+                        <div class="grey col-xs-10 col-sm-10 col-md-10 col-lg-10">
+                            <p class="grey">www.bruczek.pl</p>
+                            <p class="grey">ul. Kostki brukowej 123</p>
+                            <p class="grey">22-361 Warszawa</p>
+
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+                            <span class="grey glyphicon glyphicon-earphone phone-icon" aria-hidden="true"></span>
+                        </div>
+                        <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+                            <p class="grey">123 123 3123</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
                     <h3 class="footer-heading">Zapytaj o wycenę:</h3>                    
                     <p class="bold">Zostaw swoje zgłoszenie, a my skontaktujemy się z Tobą.</p>
                 </div>
-                <div class="col-md-offset-5 col-md-4">
+                <div class="col-xs-6 col-sm-4 col-md-offset-2 col-md-4 col-lg-offset-2 col-lg-4">
                     <h3 class="footer-heading">Znajdź nas w internecie:</h3>
                     <div class="social-icon-wrapper">
                     </div>
@@ -35,7 +49,7 @@
     </div>
     <div class="container-fluid">
         <div class="row light-gray">
-            <div class="col-md-offset-5 col-md-2 trade-mark">
+            <div class="col-xs-offset-4 col-xs-4 col-sm-offset-5 col-sm-3 col-md-offset-5 col-md-3 trade-mark">
                 <p>&#9400; 2016 www.bruczek.pl</p>
             </div>
         </div>
